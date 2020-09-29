2d0a9ca21c8c60050cd6bffbd3ea783c5681ce2d
Kate <kashleva2009@yandex.ru>
2020-09-29 11:52:50 +0300

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return(
+        <header className="header">
+            {/* <NavLink
+            to="/"
+            className="logo"
+            >
+                <img src="" />
+            <NavLink/> */}
+    
+        </header>
+    )
+}
+
+export default Header;
